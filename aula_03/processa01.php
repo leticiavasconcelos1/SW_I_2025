83373ca7724adef876a82ba8b34be2ec7d7e8591
Letícia Vasconcelos da Silva <159481123+leticiavasconcelos1@users.noreply.github.com>
2025-02-27 11:02:38 -0300

    Add files via upload

---
+++ aula_03/processa01.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = $_GET['cxnome'] ;
+    $idade = $_GET['cxidade'] ;
+
+    echo " nome digitado: $nome <br>";
+    echo " idade digitada $idade ";
+
+?>
---
+++ aula_03/processa_exer01.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = $_POST['cxnome'] ;
+    $mensage = $_POST['cxmensagem'] ;
+
+    echo "  muito obrigadapela sua mensagem $nome <br>";
+    echo " sua mensagem digitada foi: $mensage ";
+
+?>
+
